fa92fc56cf9da9baf1c563f8523c384468be6735
b0241021-spec <b0241021@ems.niu.edu.tw>
2026-07-12 19:12:09 +0800

    Delete gps-simulator-android directory

--- gps-simulator-android/app/src/main/java/com/gpssimulator/service/GPSSimulationService.kt
+++
@@ -1,7 +0,0 @@
-package com.gpssimulator.service
-import android.app.Service
-import android.content.Intent
-import android.os.IBinder
-class GPSSimulationService : Service() {
-    override fun onBind(intent: Intent?): IBinder? = null
-}
